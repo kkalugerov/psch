989eff549021181ddc089ebc18bb43773f7b65a3
Kristiyan <kkalugerov@gmail.com>
2024-07-26 23:41:43 +0300

    Update network module README

--- terraform/modules/network/provider.tf
+++ terraform/modules/network/provider.tf
@@ -4,3 +4,14 @@ provider "aws" {
     tags = var.tags
   }
 }
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.6.0"
+    }
+  }
+
+  required_version = "~> 1.1.0"
+}
