951a37aeb84aa01fe1f24e2ef32935a503fbc828
Kristiyan <kkalugerov@gmail.com>
2024-07-30 21:43:37 +0300

    Clean up

--- terraform/solution/eu-west-1/static_web/cloudfront/distribution/data.tf
+++
@@ -1,9 +0,0 @@
-data "terraform_remote_state" "static_web_origin" {
-  backend = "s3"
-
-  config = {
-    bucket = "kkalugeroff-eu-west-1-terraform-state-bucket"
-    key    = "solution/eu-west-1/static_web/origin/terraform.tfstate"
-    region = "eu-west-1"
-  }
-}
--- terraform/solution/eu-west-1/static_web/origin/state.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    encrypt = true
-    bucket  = "kkalugeroff-eu-west-1-terraform-state-bucket"
-    key     = "solution/eu-west-1/static_web/origin/terraform.tfstate"
-    region  = "eu-west-1"
-  }
-}
